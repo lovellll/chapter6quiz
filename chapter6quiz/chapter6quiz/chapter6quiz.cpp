2ec101c9f3c48dd18d8ece211f0cc9175af2bda1
Geek song <503455810@qq.com>
2017-12-14 01:26:49 +0800

    quiz1

--- chapter6quiz/chapter6quiz/chapter6quiz.cpp
+++ chapter6quiz/chapter6quiz/chapter6quiz.cpp
@@ -4,9 +4,30 @@
 #include "stdafx.h"
 #include <iostream>
 
+enum class Item
+{
+	POTION,
+	TORCHE,
+	ARROW,
+	MAXITEMS,
+};
+
+int countTotalItems(int array[])
+{
+	int count = 0;
+	for (int i = 0; i < static_cast<int>(Item::MAXITEMS); ++i)
+	{
+		count += array[i];
+	}
+	return count;
+}
+
 int main()
 {
-	
+	int array[static_cast<int>(Item::MAXITEMS)]{ 2,5,10 };
+	int sum = countTotalItems(array);
+	std::cout << "sum of items is " << sum << "\n";
+
 
     return 0;
 }
