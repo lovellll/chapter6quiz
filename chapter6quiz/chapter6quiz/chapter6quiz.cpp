d9ec44a9cf67e7092917973013735405ca2b8591
Geek song <503455810@qq.com>
2017-12-14 02:24:58 +0800

    ezone

--- chapter6quiz/chapter6quiz/chapter6quiz.cpp
+++ chapter6quiz/chapter6quiz/chapter6quiz.cpp
@@ -4,9 +4,22 @@
 #include "stdafx.h"
 #include <iostream>
 
+void pirintCstring(char *ptr)
+{
+	do
+	{
+		std::cout << *ptr;
+	} while (*(++ptr) != '\0');
+	std::cout << "\n";
+
+	
+}
+
+
 int main()
 {
-	
+	char string[]{ "Hello, world!" };
+	pirintCstring(string);
 
     return 0;
 }
