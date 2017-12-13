882eb92d49fa479468d1bb9434850458a8d16f1f
Geek song <503455810@qq.com>
2017-12-14 02:18:56 +0800

    ezone

--- chapter6quiz/chapter6quiz/chapter6quiz.cpp
+++ chapter6quiz/chapter6quiz/chapter6quiz.cpp
@@ -4,9 +4,23 @@
 #include "stdafx.h"
 #include <iostream>
 
+void swap(int &a, int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+
+}
+
+
 int main()
 {
-	
+	int x = 3, y = 4;
+	std::cout << "origin: " << x << y << "\n";
+	swap(x, y);
+	std::cout << "then " << x << y << "\n";
+
 
     return 0;
 }
